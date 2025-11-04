32d3aa92cb2217dd0d825e0c2a751e48cd263a34
Renato Nobrega <renatonobrega87@gmail.com>
2025-11-04 16:08:09 -0300

    adicionando validacoes no metodo de cadastrar uma reserva

--- src/main/java/br/com/reservasapi/services/ReservaService.java
+++ src/main/java/br/com/reservasapi/services/ReservaService.java
@@ -4,10 +4,14 @@ import br.com.reservasapi.dto.ReservaDto;
 import br.com.reservasapi.exceptions.ResourceNotFoundException;
 import br.com.reservasapi.mapper.ReservaMapper;
 import br.com.reservasapi.model.Reserva;
+import br.com.reservasapi.repositories.ClienteRepository;
+import br.com.reservasapi.repositories.QuartoRepository;
 import br.com.reservasapi.repositories.ReservaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,6 +20,8 @@ import java.util.stream.Collectors;
 public class ReservaService {
 
     private final ReservaRepository reservaRepository;
+    private final ClienteRepository clienteRepository;
+    private final QuartoRepository quartoRepository;
     private final ReservaMapper reservaMapper;
 
     public List<ReservaDto> listarTodas() {
@@ -32,8 +38,41 @@ public class ReservaService {
     }
 
     public ReservaDto salvar(ReservaDto dto) {
+
+        var cliente = clienteRepository.findById(dto.getClienteId())
+                .orElseThrow(() -> new ResourceNotFoundException("Cliente não encontrado!"));
+
+        var quarto = quartoRepository.findById(dto.getQuartoId())
+                .orElseThrow(() -> new ResourceNotFoundException("Quarto não encontrado"));
+
+        if (dto.getDataCheckIn() == null || dto.getDataCheckOut() == null) {
+            throw new IllegalArgumentException("As datas de check-in e check-out são obrigatórias");
+        }
+
+        if (!dto.getDataCheckOut().isAfter(dto.getDataCheckIn())) {
+            throw new IllegalArgumentException("A data de check-out deve ser posterior à data de check-in");
+        }
+
+        boolean quartoDisponivel = reservaRepository.findByQuartoId(quarto.getId())
+                .stream().anyMatch(reserva -> (dto.getDataCheckIn().isBefore(reserva.getDataCheckOut())
+                        && dto.getDataCheckOut().isAfter(reserva.getDataCheckIn())));
+        if (quartoDisponivel) {
+            throw new IllegalArgumentException("O quarto selecionado já está reservado nesse periodo");
+        }
+
+        long dias = ChronoUnit.DAYS.between(dto.getDataCheckIn(), dto.getDataCheckOut());
+        if (dias <= 0) {
+            throw new IllegalArgumentException("A reserva deve ser no minimo 1 dia de duração");
+        }
+        BigDecimal valorTotal = quarto.getPrecoDiaria().multiply(new BigDecimal(dias));
+        dto.setValorTotal(valorTotal);
+
+        dto.setStatus("ATIVA");
         Reserva reserva = reservaMapper.toEntity(dto);
-        return reservaMapper.toDto(reservaRepository.save(reserva));
+        reserva.setCliente(cliente);
+        reserva.setQuarto(quarto);
+        Reserva reservaSalvo = reservaRepository.save(reserva);
+        return reservaMapper.toDto(reservaSalvo);
     }
 
     public ReservaDto atualizar(Long id, ReservaDto reservaDto) {
