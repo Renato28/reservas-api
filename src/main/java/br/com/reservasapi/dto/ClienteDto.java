6b8d170dbf8f9e3185ed343076688ca2d5705223
Renato Nobrega <renatonobrega87@gmail.com>
2025-11-04 14:35:32 -0300

    adicionando validacoes no ClienteDto

--- src/main/java/br/com/reservasapi/dto/ClienteDto.java
+++ src/main/java/br/com/reservasapi/dto/ClienteDto.java
@@ -1,5 +1,8 @@
 package br.com.reservasapi.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +13,15 @@ import lombok.NoArgsConstructor;
 public class ClienteDto {
 
     private Long id;
+    @NotBlank(message = "O nome é obrigatório")
+    @Size(max = 150, message = "O nome deve ter no máximo 150 caracteres")
     private String nome;
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "E-mail inválido")
+    @Size(max = 150, message = "O e-mail deve ter no máximo 150 caracteres")
     private String email;
+    @Size(max = 30, message = "O telefone deve ter no máximo 30 caracteres")
     private String telefone;
+    @Size(max = 20, message = "O documento deve ter no máximo 20 caracteres")
     private String documento;
 }
