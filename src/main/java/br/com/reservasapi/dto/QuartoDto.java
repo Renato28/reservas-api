fb1fa8e276d9ee03283b69ed17b708cf2ac995ee
Renato Nobrega <renatonobrega87@gmail.com>
2025-11-04 14:39:22 -0300

    adicionando validacoes no QuartoDto

--- src/main/java/br/com/reservasapi/dto/QuartoDto.java
+++ src/main/java/br/com/reservasapi/dto/QuartoDto.java
@@ -1,5 +1,8 @@
 package br.com.reservasapi.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,8 +15,13 @@ import java.math.BigDecimal;
 public class QuartoDto {
 
     private Long id;
+    @NotNull(message = "O número do quarto é obrigatório")
+    @Positive(message = "O numero do quarto deve ser positivo")
     private String numero;
+    @NotBlank(message = "O tipo do quarto é obrigatório")
     private String tipo;
+    @NotNull(message = "O preço é obrigatório")
+    @Positive(message = "O preço deve ser positivo")
     private BigDecimal precoDiaria;
     private Boolean disponivel;
     private Long hotelId;
