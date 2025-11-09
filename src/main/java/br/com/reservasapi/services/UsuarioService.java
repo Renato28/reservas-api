113bba0305705ceb8e133c60451dc78ff10dff6a
Renato Nobrega <renatonobrega87@gmail.com>
2025-11-09 10:20:41 -0300

    implementando o service de usuario para cadastrar

---
+++ src/main/java/br/com/reservasapi/services/UsuarioService.java
@@ -0,0 +1,34 @@
+package br.com.reservasapi.services;
+
+import br.com.reservasapi.dto.UsuarioRequestDto;
+import br.com.reservasapi.model.Usuario;
+import br.com.reservasapi.repositories.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public Usuario cadastrar(UsuarioRequestDto dto) {
+        if (usuarioRepository.existsByEmail(dto.getEmail())) {
+            throw new RuntimeException("Já existe um usuario cadastrado com esse e-mail");
+        }
+
+        Usuario usuario = Usuario.builder()
+                .nome(dto.getNome())
+                .email(dto.getEmail())
+                .senha(passwordEncoder.encode(dto.getSenha()))
+                .role(dto.getRole())
+                .ativo(true)
+                .dataCriacao(LocalDateTime.now())
+                .build();
+        return usuarioRepository.save(usuario);
+    }
+}
