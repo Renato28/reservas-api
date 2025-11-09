1d7c05f5cd9ee2ac789654e3843e65b492b277e2
Renato Nobrega <renatonobrega87@gmail.com>
2025-11-09 17:22:09 -0300

    ajuste no endpoint de autenticacao de usuario

--- src/main/java/br/com/reservasapi/controllers/AuthController.java
+++ src/main/java/br/com/reservasapi/controllers/AuthController.java
@@ -4,10 +4,10 @@ import br.com.reservasapi.dto.LoginRequest;
 import br.com.reservasapi.dto.LoginResponse;
 import br.com.reservasapi.repositories.UsuarioRepository;
 import br.com.reservasapi.services.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,16 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Tag(name = "autenticação", description = "Autenticação de Usuários")
 public class AuthController {
 
     private final AuthService authService;
     private final UsuarioRepository usuarioRepository;
 
     @PostMapping("/login")
+    @Operation(summary = "Login de usuario", description = "Retorna o token e role do usuario logado")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) throws Exception {
-        Authentication authentication = new UsernamePasswordAuthenticationToken
-                (request.getUsername(), request.getPassword());
-        var usuario = authService.autenticar(authentication);
+        var usuario = authService.autenticar(request.getUsername(), request.getPassword());
+
         String token = authService.gerarToken(usuario);
 
         return ResponseEntity.ok(new LoginResponse(token, usuario.getRole()));
