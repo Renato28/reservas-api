10284f4907619d425aab432cb10eea196969dd25
Renato Nobrega <renatonobrega87@gmail.com>
2025-11-09 17:24:03 -0300

    implementando o JwtAuthenticationFilter

---
+++ src/main/java/br/com/reservasapi/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,50 @@
+package br.com.reservasapi.filter;
+
+import br.com.reservasapi.model.Usuario;
+import br.com.reservasapi.security.UsuarioDetailService;
+import br.com.reservasapi.security.UsuarioPrincipal;
+import br.com.reservasapi.services.AuthService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.JwtException;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final AuthService authService;
+    private UsuarioDetailService usuarioDetailService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            try {
+                String email = authService.validarToken(token);
+
+                Usuario usuario = authService.getUsuarioPorEmail(email);
+                UsuarioPrincipal principal = new UsuarioPrincipal(usuario);
+
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(principal, null, principal.getAuthorities());
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (JwtException e) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write("Token inválido");
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
