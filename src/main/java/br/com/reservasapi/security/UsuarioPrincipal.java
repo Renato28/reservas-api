4777d252a91f707ef5f954d9b87e116ea61fa4f8
Renato Nobrega <renatonobrega87@gmail.com>
2025-11-09 17:23:17 -0300

    implementando o UsuarioPrincipal do spring security

---
+++ src/main/java/br/com/reservasapi/security/UsuarioPrincipal.java
@@ -0,0 +1,55 @@
+package br.com.reservasapi.security;
+
+import br.com.reservasapi.model.Usuario;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class UsuarioPrincipal implements UserDetails {
+
+    private final Usuario usuario;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    @Override
+    public String getPassword() {
+        return usuario.getSenha();
+    }
+
+    @Override
+    public String getUsername() {
+        return usuario.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
