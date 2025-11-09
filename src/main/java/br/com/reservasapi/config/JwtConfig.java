ad84984df4627e483b679ff1ae74b727b19cac94
Renato Nobrega <renatonobrega87@gmail.com>
2025-11-09 17:25:06 -0300

    ajustando o JwtConfig

--- src/main/java/br/com/reservasapi/config/JwtConfig.java
+++ src/main/java/br/com/reservasapi/config/JwtConfig.java
@@ -5,29 +5,37 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 
-import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.security.KeyFactory;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
-import java.util.Arrays;
 import java.util.Base64;
-import java.util.HashSet;
 
 @Configuration
 public class JwtConfig {
 
+    @Value("classpath:keys/private.pem")
+    private Resource privateKeyResource;
+
+    @Value("classpath:keys/public.pem")
+    private Resource publicKeyResource;
+
     @Bean
     public JwtEncoder jwtEncoder() throws Exception {
-        RSAKey rsaKey = new RSAKey.Builder(publicKey()).privateKey(privateKey()).build();
+        RSAKey rsaKey = new RSAKey.Builder(publicKey())
+                .privateKey(privateKey())
+                .build();
         JWKSource<SecurityContext> jwkSource = new ImmutableJWKSet<>(new JWKSet(rsaKey));
         return new NimbusJwtEncoder(jwkSource);
     }
@@ -38,26 +46,22 @@ public class JwtConfig {
     }
 
     private RSAPublicKey publicKey() throws Exception {
-        try(InputStream inputStream = getClass().getClassLoader().getResourceAsStream("/keys/public.pem")) {
-            String key = new String(inputStream.readAllBytes())
-                    .replace("-----BEGIN PUBLIC KEY-----", "")
-                    .replace("-----END PUBLIC KEY-----", "");
-            byte[] decodedKey = Base64.getDecoder().decode(key);
-            return (RSAPublicKey) KeyFactory.getInstance("RSA")
-                    .generatePublic(new X509EncodedKeySpec(decodedKey));
-        }
+        String key = new String(publicKeyResource.getInputStream().readAllBytes(), StandardCharsets.UTF_8)
+                .replaceAll("-----BEGIN PUBLIC KEY-----", "")
+                .replaceAll("-----END PUBLIC KEY-----", "")
+                .replaceAll("\\s+", "");
+        byte[] decodedKey = Base64.getDecoder().decode(key);
+        return (RSAPublicKey) KeyFactory.getInstance("RSA")
+                .generatePublic(new X509EncodedKeySpec(decodedKey));
     }
 
     private RSAPrivateKey privateKey() throws Exception {
-        try(InputStream inputStream = getClass().getClassLoader().getResourceAsStream("/keys/private.pem")) {
-            String key = new String(inputStream.readAllBytes())
-                    .replace("-----BEGIN PRIVATE KEY-----", "")
-                    .replace("-----END PRIVATE KEY-----", "")
-                    .replace("\\s+", "");
-            byte[] decodedKey = Base64.getDecoder().decode(key);
-            return (RSAPrivateKey) KeyFactory.getInstance("RSA")
-                    .generatePrivate(new PKCS8EncodedKeySpec(decodedKey));
-
-        }
+        String key = new String(privateKeyResource.getInputStream().readAllBytes(), StandardCharsets.UTF_8)
+                .replaceAll("-----BEGIN PRIVATE KEY-----", "")
+                .replaceAll("-----END PRIVATE KEY-----", "")
+                .replaceAll("\\s+", "");
+        byte[] decodedKey = Base64.getDecoder().decode(key);
+        return (RSAPrivateKey) KeyFactory.getInstance("RSA")
+                .generatePrivate(new PKCS8EncodedKeySpec(decodedKey));
     }
 }
