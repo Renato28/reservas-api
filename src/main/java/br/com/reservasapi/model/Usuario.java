4f748322581eb4b5f7a3470da9f902149074f785
Renato Nobrega <renatonobrega87@gmail.com>
2025-11-09 10:20:17 -0300

    implementando a classe usuario

--- src/main/java/br/com/reservasapi/model/Usuario.java
+++ src/main/java/br/com/reservasapi/model/Usuario.java
@@ -2,6 +2,7 @@ package br.com.reservasapi.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Builder
 public class Usuario {
 
     @Id
