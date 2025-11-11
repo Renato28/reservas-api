81d965f8b820d21c05588bf1d6df4f460236dde9
Renato Nobrega <renatonobrega87@gmail.com>
2025-11-10 22:40:00 -0300

    criando o endpoint de consulta de status da reserva e usando o @PreAuthorize

--- src/main/java/br/com/reservasapi/controllers/ReservaController.java
+++ src/main/java/br/com/reservasapi/controllers/ReservaController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,24 +21,28 @@ public class ReservaController {
 
     @Operation(summary = "Listar todas as reservas", description = "Retorna uma lista completa de reservas cadastradas")
     @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'RECEPCIONISTA', 'CAMAREIRA', 'HOSPEDE')")
     public ResponseEntity<List<ReservaDto>> listarTodas() {
         return ResponseEntity.ok().body(reservaService.listarTodas());
     }
 
     @Operation(summary = "Lista as reservas pelo ID do cliente", description = "Retorna uma lista completa de reservas pelo cliente")
     @GetMapping("/{clienteId}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'RECEPCIONISTA', 'CAMAREIRA', 'HOSPEDE')")
     public ResponseEntity<List<ReservaDto>> listarPorCliente(@PathVariable Long clienteId) {
         return ResponseEntity.ok().body(reservaService.listarPorCliente(clienteId));
     }
 
     @Operation(summary = "Busca uma reserva pelo ID", description = "Retorna a reserva pelo ID informado")
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE', 'RECEPCIONISTA', 'CAMAREIRA', 'HOSPEDE')")
     public ResponseEntity<ReservaDto> buscarPorId(@PathVariable Long id) {
         return ResponseEntity.ok().body(reservaService.buscarPorId(id));
     }
 
     @Operation(summary = "Atualiza os dados de uma reserva cadastrada pelo ID", description = "Retorna o status OK de reserva atualizada com sucesso")
     @PutMapping("{id}")
+    @PreAuthorize("hasRole('RECEPCIONISTA')")
     public ResponseEntity<ReservaDto> atualizar(@PathVariable Long id, @RequestBody ReservaDto dto) {
         ReservaDto reservaAtualizada = reservaService.atualizar(id, dto);
         return ResponseEntity.ok(reservaAtualizada);
@@ -45,18 +50,25 @@ public class ReservaController {
 
     @Operation(summary = "Cadastra uma nova reserva", description = "Retorna os dados da reserva cadastrada")
     @PostMapping
+    @PreAuthorize("hasAnyRole('RECEPCIONISTA', 'HOSPEDE')")
     public ResponseEntity<ReservaDto> cadastrar(@Valid @RequestBody ReservaDto dto) {
         ReservaDto novaReserva = reservaService.cadastrar(dto);
         return  ResponseEntity.status(HttpStatus.CREATED).body(novaReserva);
     }
 
+
+    @Operation(summary = "Realiza o check-in da reserva", description = "Retorna status 200 de check-in realizado com sucesso")
     @PutMapping("/check-in/{id}")
+    @PreAuthorize("hasRole('RECEPCIONISTA')")
     public ResponseEntity<Void> realizarCheckIn(@PathVariable Long id){
         reservaService.realizarCheckIn(id);
         return ResponseEntity.ok().build();
     }
 
+
+    @Operation(summary = "Realiza o check-out da reserva", description = "Retorna status 200 de check-out realizado com sucesso")
     @PutMapping("/check-out/{id}")
+    @PreAuthorize("hasRole('RECEPCIONISTA')")
     public ResponseEntity<Void> realizarCheckOut(@PathVariable Long id){
         reservaService.realizarCheckOut(id);
         return ResponseEntity.ok().build();
@@ -64,13 +76,23 @@ public class ReservaController {
 
     @Operation(summary = "Cancela uma reserva pelo ID", description = "Retorna o status 204 de reserva cancelada com sucesso")
     @PutMapping("/cancelar/{id}")
+    @PreAuthorize("hasAnyRole('GERENTE', 'RECEPCIONISTA')")
     public ResponseEntity<Void> cancelar(@PathVariable Long id){
         reservaService.cancelar(id);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Consulta o status da reserva", description = "Retorna o status da reserva")
+    @GetMapping("/status/{id}")
+    @PreAuthorize("hasAnyRole('ADMIM', 'GERENTE', 'RECEPCIONISTA')")
+    public ResponseEntity<String> consultarStatus(@PathVariable Long id){
+        String status = reservaService.consultarStatus(id);
+        return ResponseEntity.ok().body(status);
+    }
+
     @Operation(summary = "Deleta os dados de uma reserva cadastrada pelo ID", description = "Retorna o status 204 de reserva deletada com sucesso")
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'GERENTE')")
     public ResponseEntity<Void> deletar(@PathVariable Long id) {
         reservaService.deletar(id);
         return ResponseEntity.ok().build();
