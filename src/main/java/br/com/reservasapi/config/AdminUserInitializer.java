2c36de3d3a5f1e89c5508d858e105c402c00a7f6
Renato Nobrega <renatonobrega87@gmail.com>
2025-11-14 19:49:57 -0300

    criando a classe para popular o usuario admin no banco

---
+++ src/main/java/br/com/reservasapi/config/AdminUserInitializer.java
@@ -0,0 +1,36 @@
+package br.com.reservasapi.config;
+
+import br.com.reservasapi.enums.Perfil;
+import br.com.reservasapi.model.Usuario;
+import br.com.reservasapi.repositories.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class AdminUserInitializer implements CommandLineRunner {
+
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        String adminEmail = "admin@hotel.com";
+
+        if (usuarioRepository.findByEmail(adminEmail).isEmpty()) {
+            Usuario admin = Usuario.builder()
+                    .email(adminEmail)
+                    .senha(passwordEncoder.encode("admin123"))
+                    .perfil(Perfil.ADMIN)
+                    .ativo(true)
+                    .build();
+            usuarioRepository.save(admin);
+
+            System.out.println("Usuário ADMIN criado: " + adminEmail);
+        } else {
+            System.out.println("Usuário ADMIN já existe.");
+        }
+    }
+}
