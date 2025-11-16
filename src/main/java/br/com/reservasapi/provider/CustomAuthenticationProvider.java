03ad3a584c39873fc525f4cd86d6d4de5ec3461e
Renato Nobrega <renatonobrega87@gmail.com>
2025-11-16 09:28:27 -0300

    implementando o CustomAuthenticationProvider da autenticacao do usuario

---
+++ src/main/java/br/com/reservasapi/provider/CustomAuthenticationProvider.java
@@ -0,0 +1,48 @@
+package br.com.reservasapi.provider;
+
+import br.com.reservasapi.model.Usuario;
+import br.com.reservasapi.repositories.UsuarioRepository;
+import br.com.reservasapi.security.UsuarioPrincipal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    private final UsuarioRepository  usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String email = authentication.getName();
+        String senha = authentication.getCredentials().toString();
+
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+        if (!passwordEncoder.matches(senha, usuario.getSenha())) {
+            throw new BadCredentialsException("Credenciais inválidas");
+        }
+
+        UsuarioPrincipal principal = new UsuarioPrincipal(usuario);
+
+        return new UsernamePasswordAuthenticationToken(
+                principal,
+                senha,
+                principal.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
