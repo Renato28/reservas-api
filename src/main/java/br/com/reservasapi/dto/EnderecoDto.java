37310efd4c5f68167f91b8c68adda0b5f9b67dda
Renato Nobrega <renatonobrega87@gmail.com>
2025-12-20 18:17:12 -0300

    implementando o dto de endereco

---
+++ src/main/java/br/com/reservasapi/dto/EnderecoDto.java
@@ -0,0 +1,14 @@
+package br.com.reservasapi.dto;
+
+import lombok.Data;
+
+@Data
+public class EnderecoDto {
+    private String cep;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+}
