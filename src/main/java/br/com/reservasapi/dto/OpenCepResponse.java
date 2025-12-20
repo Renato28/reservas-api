625782f542f30c0c1323e18fa5e7169e2e5f726a
Renato Nobrega <renatonobrega87@gmail.com>
2025-12-20 18:16:01 -0300

    implementando response do cep

---
+++ src/main/java/br/com/reservasapi/dto/OpenCepResponse.java
@@ -0,0 +1,21 @@
+package br.com.reservasapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class OpenCepResponse {
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String unidade;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String estado;
+    private String regiao;
+    private String ibge;
+}
